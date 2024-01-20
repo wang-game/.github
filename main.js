c81f8d12a5bb7e4bde6c51d92f4f38399dc7b980
gyahury <rudgns1243@naver.com>
2024-01-20 17:34:09 +0900

    Feat: 장애물 움직임 처리

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let timer = 0;
+let obsArr = [];
 
-canvas.width = window.innerWidth - 100;
+canvas.width = 500;
 canvas.height = window.innerHeight - 100;
 
-// 캐릭터
-
-let character = {
-  x: 10,
+const character = {
+  x: 60,
   y: 300,
   width: 50,
   height: 50,
@@ -17,17 +17,41 @@ let character = {
   },
 };
 
-character.draw();
-
-// // 장애물
 class Obs {
   constructor() {
-    (this.x = 400), (this.y = 300), (this.width = 50), (this.height = 50);
+    (this.x = 600), (this.y = 300), (this.width = 50), (this.height = 50);
   }
   draw() {
     ctx.fillStyle = "red";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
-let obs = new Obs();
-obs.draw();
+
+
+moveAction();
+
+function moveAction() {
+  requestAnimationFrame(moveAction);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  timer++;
+  if (timer % 120 == 0) {
+    let obs = new Obs();
+    obsArr.push(obs);
+  }
+  obsArr.forEach((obs, index) => {
+    obs.x -= 5; 
+    if (obs.x + obs.width < 0) {
+      obsArr.splice(index, 1);
+    }
+    obs.draw();
+  });
+
+  // draw line
+  ctx.beginPath();
+  ctx.moveTo(0, 350);
+  ctx.lineTo(500, 350);
+  ctx.stroke();
+
+  character.draw();
+}
