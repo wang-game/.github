a291b229da40c1372eda532ec14e4605354607db
dhkdtjs1541 <dhkdtjs1541@naver.com>
2024-01-20 16:21:12 +0900

    Style: character draw

--- main.js
+++ main.js
@@ -2,36 +2,32 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight -100;
+canvas.height = window.innerHeight - 100;
 
 // 캐릭터
 
 let character = {
-    x : 10,
-    y : 300,
-    width : 50,
-    height : 50,
-    draw(){     
+  x: 10,
+  y: 300,
+  width: 50,
+  height: 50,
+  draw() {
     ctx.fillStyle = "green";
-    ctx.fillRect (this.x, this.y, this.width, this.height);
-    }
-}
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+};
 
 character.draw();
 
 // // 장애물
-// class obs {
-//     constructor() {
-//         this.x = 500,
-//         this.y = 300,
-//         this.width = 50,
-//         this.height = 50
-//     }
-//         draw() {
-//             ctx.fillStyle = "red"
-//             ctx.fillRect(this.x, this,y, this.width, this.height);
-//         }
-//     };
-
-//     let obs = new obs();
-//     obs.draw();
+class Obs {
+  constructor() {
+    (this.x = 400), (this.y = 300), (this.width = 50), (this.height = 50);
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+let obs = new Obs();
+obs.draw();
