9cb14f2bde1612d875f9244c097165ae6c2fc1ab
dhkdtjs1541 <dhkdtjs1541@naver.com>
2024-01-20 15:16:29 +0900

    Layout: canvas set

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight -100;
+
+
+// 캐릭터
